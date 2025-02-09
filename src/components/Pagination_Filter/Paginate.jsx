9d578dcce5bb5dbd6aa5cf3d7d67527e6d192cf4
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-02-09 22:50:27 +0530

    pg2

--- src/components/Pagination_Filter/Paginate.jsx
+++ src/components/Pagination_Filter/Paginate.jsx
@@ -1,50 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 const Paginate = () => {
-
-    const[productList,setProductList] = useState([])
-    const[categoryList,setCategoryList] = useState([])
-    
-    const fetchProducts = async()=>{
-        const response = await fetch("https://dummyjson.com/products",{method: "GET"})
-        const data = await response.json();
-
-        setProductList(data.products)
-        setCategoryList([...new Set(data.products.map((item) => item.category))]);
-
-        console.log("List fetched",data.products)
-        console.log("categories List fetched",categoryList) 
-     }  
- 
-     useEffect(()=>{ 
-
-        fetchProducts()
-     },[])
-
-    return (
-        <>
-    <select >
-        <option>Select category</option>
-        {
-            categoryList.map((category,index)=>(
-                <option id='index' value={category}>
-                    {category}
-                </option>
-            ))
-        }
-    </select>
-
-        
+  return (
     <div>
-      {
-        productList.map((product,index)=>(
-            <>
-                <p key={index}>{product.title}</p>
-            </>
-        ))
-      }
+      
     </div>
-    </>
   )
 }
 
